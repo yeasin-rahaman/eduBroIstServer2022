1c2ecf47d1701e1ac76836a8b05099f3c37f20a0
Dev70 <Shanto9585>
2022-07-23 01:30:54 +0600

    update

--- Utility/Cloudinary.js
+++ Utility/Cloudinary.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const cloudinary = require('Cloudinary').v2;
+const cloudinary = require('cloudinary').v2;
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,

c8177092cd09cb05b5a109e755d615cd4dd7967b
Dev70 <Shanto9585>
2022-11-01 00:19:08 +0600

    update

--- index.js
+++ index.js
@@ -301,7 +301,7 @@ async function run() {
 
         // Get all Assignments api 
         app.get("/getAllAssignments", async (req, res) => {
-            const cursor = allAssignmentsCollection.find({});
+            const cursor = allAssignmentsCollection.find(query, status = "approved");
             const allAssignments = await cursor.toArray();
             res.json(allAssignments);
         });
